8b6cd1d6f2a6d8b4584d258c3e68dd46e0de6805
Rintel <ced.laier@gmail.com>
2018-10-12 16:06:55 +0200

    add operation arguments

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,34 +2,116 @@ import { GraphQLServer } from 'graphql-yoga';
 
 // Scalar types - String, Boolean, Int, Float, ID
 
+//Demo user data 
+const users = [{
+	id: 1,
+	name: 'Cedric',
+	email: 'cedric@example.com',
+	age: 28
+}, {
+	id: 2,
+	name: 'Sarah',
+	email: 'sarah@example.com'
+}, {
+	id: 3,
+	name: 'Marcin',
+	email: 'marcin@example.com'
+}]
+
+const posts = [{
+	id: '1',
+	title: 'title a',
+	body: 'body a' ,
+	published: true
+}, {
+	id: '2',
+	title: 'title b',
+	body: 'body b' ,
+	published: false
+}, {
+	id: '3',
+	title: 'title c',
+	body: 'body c' ,
+	published: true
+}]
+
 // Type definitions (schema)
 const typeDefs = `
 	type Query {
+		users(query: String): [User!]! 
+		posts(posts: String): [Post!]!
+		greeting(name: String, position: String): String!
+		add(numbers: [Float!]!): Float!
+		grades: [Int]!
+		sven: User! 
+		post: Post!
+	}
+
+	type User {
+		id: ID!
+		name: String!
+		email: String!
+		age: Int
+	}
+
+	type Post {
+		id: ID!
 		title: String!
-		price: Float!
-		releaseYear: Int
-		rating: Float
-		inStock: Boolean!
+		body: String!
+		published: Boolean!
 	}
 ` 
 
 // Resolvers
 const resolvers = {
 	Query: {
-		title() {
-			return 'Ilai the greatest dog alive'
+		users(parent, args, ctx, info) {
+			if (!args.query) {
+				return users
+			}
+
+			return users.filter((user) => {
+				return user.name.toLowerCase().includes(args.query.toLowerCase())
+			})
 		},
-		price() {
-			return 99.99
+		posts(parent, args, ctx, info) {
+			return posts
 		},
-		releaseYear() {
-			return null
+		greeting(parent, args, ctx, info) {
+			if (args.name && args.position) {
+				return `Hello ${args.name}! You are my favourite ${args.position}.`	
+			} else {
+				return 'Hello!'
+			}
 		},
-		rating() {
-			return 10.0
+		add(parent, args, ctx, info) {
+			if(args.numbers.length === 0) {
+				return 0
+			}
+
+			return args.numbers.reduce((acc, curr)=> {
+				return acc + curr
+			})			
 		},
-		inStock() {
-			return true
+		grades(parent, args, ctx, info) {
+			return [99, 80, 93]
+		},
+		sven() {
+			return {
+				//that's the place where you can perfom db actions, atm still hard coded :P
+				id: 'abc123',
+				name: 'Sven',
+				email: 'sven@example.com',
+				age: 26
+			}
+		},
+		post() {
+			return {
+				id: 'cba321',
+				title: 'Cedrics GraphQL Workshop',
+				body: 'Fuck me, that is awesome!',
+				published: true
+			}
 		}
 	}
 }
